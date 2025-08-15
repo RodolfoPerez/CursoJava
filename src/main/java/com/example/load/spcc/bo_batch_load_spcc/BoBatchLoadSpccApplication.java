28a4c9c211995302b1e15567eb95a474ef16bd63
Rodolfo <rperezh2@eglobal.com.mx>
2025-08-15 14:02:27 -0600

    Clase - 15/08/2025

--- src/main/java/com/example/load/spcc/bo_batch_load_spcc/BoBatchLoadSpccApplication.java
+++ src/main/java/com/example/load/spcc/bo_batch_load_spcc/BoBatchLoadSpccApplication.java
@@ -1,17 +1,90 @@
 package com.example.load.spcc.bo_batch_load_spcc;
 
+import com.example.load.spcc.bo_batch_load_spcc.utils.BatchUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.github.lalyos.jfiglet.FigletFont;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 @Slf4j
-public class BoBatchLoadSpccApplication {
+public class BoBatchLoadSpccApplication implements ApplicationRunner {
 
-    public static void main(String[] args) {
 
-        SpringApplication.run(BoBatchLoadSpccApplication.class, args);
+
+    private static Job finalExchangeStep1;
+    private static Job finalExchangeStep2;
+    private static Job finalExchangeStepALL;
+    private static JobLauncher jobLauncher;
+
+    public BoBatchLoadSpccApplication(Job finalExchangeStep1,Job finalExchangeStep2,Job finalExchangeStepALL,JobLauncher jobLauncher){
+
+        this.finalExchangeStep1= finalExchangeStep1;
+        this.finalExchangeStep2= finalExchangeStep2;
+        this.finalExchangeStepALL= finalExchangeStepALL;
+        this.jobLauncher= jobLauncher;
 
     }
 
+    public static void main(String[] args) {
+
+        System.exit(SpringApplication.exit(SpringApplication.run(BoBatchLoadSpccApplication.class, args)));
+
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        log.info("\n" + FigletFont.convertOneLine("Curso Java"));
+        log.info("Total de parametros recibidos: {}", args.getSourceArgs().length - 1);
+
+
+        String acquirer = BatchUtils.getArgumentValue(args,"acquirer");
+        LocalDate dateProcess = BatchUtils.getArgumentValueAsLocalDate(args,"dateProcess", "yyyyMMdd");
+        String step = BatchUtils.getArgumentValue(args,"step").toUpperCase();
+
+        if (!step.equals("1") && !step.equals("2") && !step.equals("ALL")) {
+            log.error("Step desconocido: {}", step);
+            System.exit(1);
+        }
+
+        log.info("=====================================");
+        log.info("|  Ejecutando batch con parámetros:  ");
+        log.info("|  ----------------|---------------");
+        log.info("|  Fecha proceso   |   {}              ", dateProcess);
+        log.info("|  Adquirente      |   {}              ", acquirer);
+        log.info("|  Step            |   {}              ", step);
+        log.info("=====================================");
+
+
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLocalDate("dateProcess",dateProcess)
+                .addString("acquirer",acquirer)
+                .toJobParameters();
+
+        switch(step){
+            case "1":
+                this.jobLauncher.run(this.finalExchangeStep1,jobParameters);
+                break;
+            case "2":
+                this.jobLauncher.run(this.finalExchangeStep2,jobParameters);
+                break;
+            case "ALL":
+                this.jobLauncher.run(this.finalExchangeStepALL,jobParameters);
+                break;
+            default:
+                log.error("Step desconocido {}" , step);
+                System.exit(1);
+        }
+    }
 }
+
